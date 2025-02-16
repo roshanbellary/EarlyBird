268fd781b2865a13346b9c6e1bcda0f87e46e0ea
eshansinghal14 <eshansinghal05@gmail.com>
2025-02-15 23:59:24 -0800

    finished user_ask_expert

--- backend/podcast/agents/pipeline.py
+++ backend/podcast/agents/pipeline.py
@@ -97,7 +97,7 @@ class NewsPodcastPipeline:
         mistral_api_key=os.getenv("MISTRAL_API_KEY")
         script_generator = PodcastScriptGenerator(mistral_api_key)
         script_generator.chat_history.append(f"<HOST{i}>{question}</HOST{i}>")
-        ans = script_generator.generate_response(self.script_generator.expert_chain, stories[i / 2]["story"][0]['draft'])
+        ans = script_generator.generate_response(script_generator.expert_chain, stories[i / 2]["story"][0]['draft'])
 
         audio_generator = PodcastAudioGenerator()
         interrupt_path = filepath[:filepath.rfind('\\')] + '\\' + 'podcast_interrupt_' + str(i) + '.mp3'
